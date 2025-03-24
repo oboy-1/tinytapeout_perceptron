5c5648d8f4db7ff053dc4586aa8c0c1db41742f6
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2025-03-24 18:28:30 -0400

    Better loop + reading

--- src/ternary_perceptron.v
+++ src/ternary_perceptron.v
@@ -1,29 +1,29 @@
 module perceptron (
-  input wire [7:0] weights,
+  input wire [7:0] weights, // but only use [3:0]
   input wire [7:0] inputs,
-  output reg [7:0] out,
+  output reg [7:0] out, // but only write to [3:0]
   
   input wire       clk,
   input wire       reset
 );
 
     integer i;
-    reg signed [15:0] sum;  // Temporary accumulation register
+    reg signed [3:0] sum;  // Temporary accumulation register
 
     always @(posedge clk) begin
       if (reset) begin
       	out <= 0;
       end else begin
       	sum = 0; // Reset sum on each clock cycle
-      	for (i = 0; i < 7; i = i + 2) begin // Read every two bits as one of three options
-      	  crumb = {i + 1, i};
+        for (i = 0; i < 4; i = i + 1) begin // Read every two bits as one of three options
+          crumb = {2 * i + 1, 2 * i};
       	  case(crumb)
       	    2'b01: sum = sum + inputs[i]; // 01 -> +1
       	    2'b11: sum = sum - inputs[i]; // 11 -> -1
       	    default: ; // 00 || 10 -> 0 (pass)
       	  endcase
         end
-      	out <= sum[7:0]; // Assign final accumulated value to output
+      	out <= sum; // Automatic zero padding takes care of the 4 left bits
       end
     end
 
