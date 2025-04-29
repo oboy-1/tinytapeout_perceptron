96b0df78fe304926b9653ce3de26832a136602da
Karthik S. Vedula <karthik@vedula.me>
2025-04-29 14:19:55 -0400

    works

--- src/ternary_perceptron.v
+++ src/ternary_perceptron.v
@@ -1,4 +1,4 @@
-module perceptron (
+module ternary_perceptron (
   input wire [7:0] weights,
   input wire [7:0] inputs, // but only read [3:0]
   output reg [7:0] out, // but only write to [3:0]
@@ -17,7 +17,7 @@ module perceptron (
       end else begin
       	sum = 0; // Reset sum on each clock cycle
         for (i = 0; i < 4; i = i + 1) begin // Read every two bits as one of three options
-          crumb = {2 * i, 2 * i + 1};
+          crumb = weights[2*i +: 2]; // this means: take 2 bits starting at 2*i
       	  case(crumb)
       	    2'b01: sum = sum + inputs[i]; // 01 -> +1
       	    2'b11: sum = sum - inputs[i]; // 11 -> -1
